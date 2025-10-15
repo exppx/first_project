c801754d3e2a920fa20766da1592e2611b5c49d1
Gleb Morin <moringleb183@gmail.com>
2025-10-15 12:06:26 +0500

    docs: setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",  // вставляет стили в DOM
+          "css-loader",    // позволяет импортировать CSS
+          "sass-loader"    // компилирует SCSS → CSS
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+    open: true,
+    hot: true,
+    port: 3000,
+  },
+  mode: "development"
+};
